fc393e59cacb440e81ed0907cffa86d2a219caf1
Yash Pratap Singh <singhyashpratap06@gmail.com>
2019-01-08 00:29:06 +0530

    conditional rendering and styling added

--- src/App.js
+++ src/App.js
@@ -1,40 +1,83 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css';                             //Webpack allows us to import CSS files into JS files. 
 import Person from './Person/Person';
 
 class App extends Component {
   state = {
     persons : [
-      { name: 'Alpha', age : '28'},
-      { name : 'Bravo', age : '27'},
-      { name : 'Charlie', age : '25'}
-    ]
+      {id : 'afd', name: 'Alpha', age : '28'},
+      {  id : 'dfjsdkf' ,name : 'Bravo', age : '27'},
+      {id : 'afj', name : 'Charlie', age : '25'}
+    ],
+    otherState : 'some other value',
+    showPersons : false
   };
 
-  switchNameHandler = () => {
-    // console.log('Was clicked!');
-    // DONT'T do this => this.state.persons[0].name = "Maximilian";
-    this.setState( {
-      persons : [
-        { name: 'Yash', age : '28'},
-        { name : 'Bravo', age : '27'},
-        { name : 'Charlie', age : '21'}
-      ]
-    }); 
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons});
+  } 
+  nameChangedHandler = (event ,id) => {
+    const personIndex = this.state.persons.findIndex( p => {
+      return p.id === id;
+    });
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const  persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({persons});
   }
+                                              //Object.assign({},obj) to copy object!!
+                                              //... to copy array/object !!!
+  togglePersonsHandler = ()=> {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons : !doesShow
+    });
+  }
+    
 
   render() {
+    const style = {
+      backgroundColor: 'green',
+      color : 'white',
+      font : 'inherit',
+      border : '1px solid blue',
+      padding : '8px',
+      cursor : 'pointer'
+    }
+
+    let classes = [];
+    if(this.state.persons.length <=2){
+      classes.push('red');
+    }
+    if(this.state.persons.length<=1){
+      classes.push('bold');
+    }
+
+    let persons = null;                //Standard way to conditionally render elements. Keep the display logic outside the JSX block in render().
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map( (person,index) => {
+            return <Person click = {()=>this.deletePersonHandler(index)} name = {person.name} age = {person.age} key = {person.id} changed = {(event)=>this.nameChangedHandler(event, person.id)} />
+          })}
+        </div> 
+      )
+      style.backgroundColor = 'red';
+    }
     return (
       <div className="App">
         <h1>Hi I'm React App</h1>
-        <p>This is really working !</p>
-        <button onClick = {this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age = {this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age = {this.state.persons[1].age} click= {this.switchNameHandler} >My hobbies are: Skiing</Person>
-        <Person name={this.state.persons[2].name} age = {this.state.persons[2].age} />
+        <p className = {classes.join(' ')}>This is really working !</p>
+        <button style = {style} onClick = {this.togglePersonsHandler}  >Toggle Persons</button>
+        {persons}                                                        
       </div>
     );
     // return React.createElement('div', {className : 'App' }, React.createElement('h1', null, 'Hi I\'m a React App'));
+    // No complex elements inside this { dynamic content brace }
   }
 }
 
